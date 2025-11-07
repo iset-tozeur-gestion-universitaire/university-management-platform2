900173281631d2cbd859cc052d32dd921d1f43e6
ayaborken123 <borkenaya90@gmail.com>
2025-11-07 01:29:43 +0100

    modification de  crud enseignant

--- backend/admin-service/src/enseignant/enseignant.entity.ts
+++ backend/admin-service/src/enseignant/enseignant.entity.ts
@@ -1,6 +1,18 @@
 // src/enseignant/enseignant.entity.ts
-import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  ManyToMany,
+  OneToMany,
+  JoinTable,
+} from 'typeorm';
 import { Departement } from '../departement/entities/departement.entity';
+import { Specialite } from '../specialite/entities/specialite.entity';
+import { Classe } from '../classe/entities/classe.entity';
+//import { Matiere } from '../matiere/matiere.entity';
+
 @Entity()
 export class Enseignant {
   @PrimaryGeneratedColumn()
@@ -15,8 +27,30 @@ export class Enseignant {
   @Column({ unique: true })
   email: string;
 
+  @Column({ nullable: true })
+  grade: string;
+
+  // 🔹 Un enseignant appartient à un seul département
   @ManyToOne(() => Departement, (departement) => departement.enseignants, {
     onDelete: 'CASCADE',
   })
   departement: Departement;
+
+  // 🔹 Un enseignant peut enseigner dans plusieurs spécialités
+  @ManyToMany(() => Specialite, (specialite) => specialite.enseignants, {
+    cascade: true,
+  })
+  @JoinTable()
+  specialites: Specialite[];
+
+  // 🔹 Un enseignant peut enseigner dans plusieurs classes
+  @ManyToMany(() => Classe, (classe) => classe.enseignants, {
+    cascade: true,
+  })
+  @JoinTable()
+  classes: Classe[];
+
+  // 🔹 Un enseignant enseigne plusieurs matières
+  //@OneToMany(() => Matiere, (matiere) => matiere.enseignant)
+  //matieres: Matiere[];
 }
--- backend/admin-service/src/enseignant/enseignant.module.ts
+++ backend/admin-service/src/enseignant/enseignant.module.ts
@@ -1,14 +1,17 @@
-// src/enseignant/enseignant.module.ts
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Enseignant } from './enseignant.entity';
-import { Departement } from '../departement/entities/departement.entity';
 import { EnseignantService } from './enseignant.service';
 import { EnseignantController } from './enseignant.controller';
-
+import { Enseignant } from './enseignant.entity';
+import { Departement } from '../departement/entities/departement.entity';
+import { Classe } from '../classe/entities/classe.entity';
+import { Specialite } from '../specialite/entities/specialite.entity';
 @Module({
-  imports: [TypeOrmModule.forFeature([Enseignant, Departement])],
+  imports: [
+    TypeOrmModule.forFeature([Enseignant, Departement, Specialite, Classe]),
+  ],
   controllers: [EnseignantController],
   providers: [EnseignantService],
+  exports: [EnseignantService],
 })
 export class EnseignantModule {}
