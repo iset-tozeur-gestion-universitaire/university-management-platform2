3a4ef216ca71e7dc0a2a6390e843dea170965b63
KhaledZeineb <khaledzeineb81@gmail.com>
2025-11-19 16:51:28 +0100

    chore: remove node_modules from repo and add to .gitignore

--- node_modules/admin-service/src/enseignant/dto/update-enseignant.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateEnseignantDto } from './create-enseignant.dto';
-
-export class UpdateEnseignantDto extends PartialType(CreateEnseignantDto) {}
--- node_modules/admin-service/src/matiere/dto/update-matiere.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateMatiereDto } from './create-matiere.dto';
-
-export class UpdateMatiereDto extends PartialType(CreateMatiereDto) {}
